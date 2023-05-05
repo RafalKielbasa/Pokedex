c9646d382da85e4ab4282bf19fd720053299559f
Mateusz Kowa <m.kowa2424@gmail.com>
2023-05-05 08:08:56 +0200

    05.05-detailedTest

--- pokedex/src/Pages/DetailedPage.js
+++ pokedex/src/Pages/DetailedPage.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { DetailedPokemonCard } from "./components";
 
 const DetailedPage = () => {
-  return <h1>DetailedPage</h1>;
+  return <DetailedPokemonCard />;
 };
 
 export default DetailedPage;
--- pokedex/src/Pages/components/index.js
+++ pokedex/src/Pages/components/index.js
@@ -2,3 +2,4 @@ export { default as Searcher } from "./Searcher";
 export { default as PokemonCard } from "./PokemonCard";
 export { default as PokemonCardContainer } from "./PokemonCardContainer";
 export { default as MyPagination } from "./MyPagination";
+export { default as DetailedPokemonCard } from "./DetailedPokemonCard";
