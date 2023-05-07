28d47f0dd7be193625224cef9af6635ccb3deddd
Mateusz Kowa <m.kowa2424@gmail.com>
2023-05-07 19:16:54 +0200

    przeniesienie plików

--- pokedex/src/Pages/components/index.js
+++ pokedex/src/Pages/components/index.js
@@ -1,6 +1,6 @@
 export { default as Searcher } from "./Searcher";
 export { default as PokemonCard } from "./Cards/PokemonCard";
 export { default as PokemonCardContainer } from "./Cards/PokemonCardContainer";
-export { default as MyPagination } from "./MyPagination";
+export { default as MyPagination } from "./ExlusiveHomePageComponents/MyPagination";
 export { default as DetailedPokemonCard } from "./Cards/DetailedPokemonCard";
 export { default as DetailedPokemonCardConatiner } from "./Cards/DetailedPokemonCardConatiner";
