110968c0dbef09f9f42fd63e2fa697254778669d
Wiktor Ostrach <ostrach109@gmail.com>
2023-05-07 02:45:58 +0200

    register and login stage1

--- src/pages/Logowanie.jsx
+++ src/pages/Logowanie.jsx
@@ -1,3 +1,137 @@
-export default function Home() {
-  return <h1>Logowanie</h1>;
+import { useState, useEffect } from "react";
+import axios from "axios";
+import * as Yup from "yup";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
+import styled from "styled-components";
+
+const Container2 = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const FormWrapper = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid black;
+  padding: 50px;
+`;
+
+const userSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Nieprawidłowy adres e-mail.")
+    .required("Adres e-mail jest wymagany."),
+  password: Yup.string().required("Hasło jest wymagane."),
+});
+
+export default function Logowanie() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [userInfo, setUserInfo] = useState([]);
+  const [test, setTest] = useState([]);
+  // console.log("rejestr", test);
+  // console.log("userInfo", userInfo);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/user")
+      .then((response) =>
+        setTest(
+          response.data?.filter((item) => {
+            return item.name && item.password;
+          })
+        )
+      )
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleOnSubmit = (values, actions) => {
+    setIsSubmitting(true);
+    console.log(values);
+    actions.setSubmitting(false);
+    setUserInfo(values);
+    checkLogin(values);
+  };
+
+  const checkLogin = (gwo) => {
+    const currLog = gwo.email;
+    const currPass = gwo.password;
+    const filtered = test.filter((item) => {
+      console.log("dupa", item);
+      return item.email === currLog;
+    });
+    console.log(filtered);
+
+    if (filtered[0].password === currPass && filtered[0].email === currLog) {
+      console.log("zalogowano");
+    } else {
+      console.log("bledne haslo lub login");
+    }
+  };
+
+  return (
+    <Container
+      sx={{
+        marginTop: "30px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "300px",
+        border: "1px solid black",
+      }}
+    >
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        onSubmit={handleOnSubmit}
+        validationSchema={userSchema}
+      >
+        {({ errors, touched, isSubmitting }) => (
+          <Form>
+            <Box marginBottom={2}>
+              <h1>Logowanie</h1>
+            </Box>
+            <Box marginBottom={2}>
+              <Field
+                as={TextField}
+                type="email"
+                name="email"
+                label="Adres e-mail"
+                error={touched.email && errors.email ? true : false}
+                helperText={touched.email && errors.email ? errors.email : ""}
+              />
+            </Box>
+            <Box marginBottom={2}>
+              <Field
+                as={TextField}
+                type="password"
+                name="password"
+                label="Hasło"
+                fullWidth
+                error={touched.password && errors.password ? true : false}
+                helperText={
+                  touched.password && errors.password ? errors.password : ""
+                }
+              />
+            </Box>
+            <Box display="flex" justifyContent="center" marginBottom={2}>
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                disabled={isSubmitting}
+              >
+                Zaloguj
+              </Button>
+            </Box>
+          </Form>
+        )}
+      </Formik>
+    </Container>
+  );
 }
--- src/pages/Rejestracja.jsx
+++ src/pages/Rejestracja.jsx
@@ -1,3 +1,143 @@
-export default function Home() {
-  return <h1>Rejestracja</h1>;
+import { useState } from "react";
+import axios from "axios";
+import * as Yup from "yup";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
+import styled from "styled-components";
+
+const Container2 = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const FormWrapper = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid black;
+  padding: 50px;
+`;
+
+const userSchema = Yup.object().shape({
+  name: Yup.string().required("Imię jest wymagane."),
+  email: Yup.string()
+    .email("Nieprawidłowy adres e-mail.")
+    .required("Adres e-mail jest wymagany."),
+  password: Yup.string()
+    .min(6, "Hasło musi składać się z co najmniej 6 znaków.")
+    .required("Hasło jest wymagane."),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password"), null], "Hasła muszą się zgadzać.")
+    .required("Potwierdź hasło."),
+});
+
+export default function Rejestracja() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleOnSubmit = (values, actions) => {
+    setIsSubmitting(true);
+    console.log(values);
+    actions.setSubmitting(false);
+    axios.post(`http://localhost:3001/user/`, {
+      name: values.name,
+      email: values.email,
+      password: values.password,
+    });
+  };
+
+  return (
+    <Container
+      sx={{
+        marginTop: "30px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "300px",
+        border: "1px solid black",
+      }}
+    >
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+        }}
+        onSubmit={handleOnSubmit}
+        validationSchema={userSchema}
+      >
+        {({ errors, touched, isSubmitting }) => (
+          <Form>
+            <Box marginBottom={2}>
+              <h1>Rejestracja</h1>
+            </Box>
+            <Box marginBottom={2}>
+              <Field
+                as={TextField}
+                name="name"
+                label="Imię"
+                error={touched.name && errors.name ? true : false}
+                helperText={touched.name && errors.name ? errors.name : ""}
+              />
+            </Box>
+            <Box marginBottom={2}>
+              <Field
+                as={TextField}
+                type="email"
+                name="email"
+                label="Adres e-mail"
+                error={touched.email && errors.email ? true : false}
+                helperText={touched.email && errors.email ? errors.email : ""}
+              />
+            </Box>
+            <Box marginBottom={2}>
+              <Field
+                as={TextField}
+                type="password"
+                name="password"
+                label="Hasło"
+                fullWidth
+                error={touched.password && errors.password ? true : false}
+                helperText={
+                  touched.password && errors.password ? errors.password : ""
+                }
+              />
+            </Box>
+            <Box marginBottom={2}>
+              <Field
+                as={TextField}
+                type="password"
+                name="confirmPassword"
+                label="Potwierdź hasło"
+                fullWidth
+                error={
+                  touched.confirmPassword && errors.confirmPassword
+                    ? true
+                    : false
+                }
+                helperText={
+                  touched.confirmPassword && errors.confirmPassword
+                    ? errors.confirmPassword
+                    : ""
+                }
+              />
+            </Box>
+            <Box display="flex" justifyContent="center" marginBottom={2}>
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                disabled={isSubmitting}
+              >
+                Zarejestruj
+              </Button>
+            </Box>
+          </Form>
+        )}
+      </Formik>
+    </Container>
+  );
 }
