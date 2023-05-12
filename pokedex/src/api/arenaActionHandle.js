cd1a728c96d5871d4f097b25468127177d471f62
Mateusz Kowa <m.kowa2424@gmail.com>
2023-05-12 15:02:52 +0200

    patched not working

--- pokedex/src/api/arenaActionHandle.js
+++ pokedex/src/api/arenaActionHandle.js
@@ -4,23 +4,27 @@ export const arenaActionHandle = async (location, data, myId) => {
     ...data,
     base_experience: data?.data?.base_experience + 10,
   };
-
   try {
     const response = await axios.patch(
-      `http://localhost:3000/${location}/${myId}/`,
+      `http://localhost:3000/${location}/${myId}`,
       {
         data: newData,
       }
     );
+    console.log("Patched");
     return response;
   } catch (error) {
     if (error.response.status === 404) {
+      const newData = {
+        ...data,
+        base_experience: data?.data?.base_experience + 10,
+      };
       const response = await axios.post(`http://localhost:3000/${location}/`, {
         data: newData,
         id: myId,
       });
+      console.log("POST");
       return response;
     }
   }
-  console.log({ newData });
 };
