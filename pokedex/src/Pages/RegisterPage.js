dce31d66fb3e9b4e574da171f0eeb989a44b9b66
Mateusz Kowa <m.kowa2424@gmail.com>
2023-05-15 09:56:35 +0200

    formularz rejestracji ver 1.1

--- pokedex/src/Pages/RegisterPage.js
+++ pokedex/src/Pages/RegisterPage.js
@@ -1,59 +1,67 @@
-import { useFormik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import React from "react";
+import * as Yup from "yup";
 
 const RegisterPage = () => {
-  const formik = useFormik({
-    initialValues: {
-      userName: "",
-      email: "",
-      password: "",
-      repeatPassword: "",
-    },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
-  console.log({ formik });
   return (
-    <form onSubmit={formik?.handleSubmit}>
-      <label htmlFor="userName">Nazwa Użytkownika</label>
-      <input
-        id="userName"
-        name="userName"
-        type="text"
-        placeholder="Wprowadź nazwę użytkownika"
-        onChange={formik?.handleChange}
-        value={formik?.values?.userName}
-      />
-      <label htmlFor="email">E-mail</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        placeholder="Wprowadź email"
-        onChange={formik?.handleChange}
-        value={formik?.values?.email}
-      />
-      <label htmlFor="password">Hasło</label>
-      <input
-        id="password"
-        name="password"
-        type="password"
-        placeholder="Wprowadź hasło"
-        onChange={formik?.handleChange}
-        value={formik?.values?.password}
-      />
-      <label htmlFor="repeatPassword">Powtórz Hasło</label>
-      <input
-        id="repeatPassword"
-        name="repeatPassword"
-        type="password"
-        placeholder="Powtórz hasło"
-        onChange={formik?.handleChange}
-        value={formik?.values?.repeatPassword}
-      />
-      <button type="submit">Wyślij</button>
-    </form>
+    <>
+      <Formik
+        initialValues={{
+          userName: "",
+          email: "",
+          password: "",
+          repeatPassword: "",
+        }}
+        validationSchema={Yup.object({
+          userName: Yup.string()
+            .max(15, "Must be 15 characters or less")
+            .required("Required"),
+          email: Yup.string()
+            .email("Invalid email address")
+            .required("Required"),
+          password: Yup.string()
+            .matches(
+              "^(?=.*[A-Za-z])(?=.*d)(?=.*[@$!%*#?&])[A-Za-zd@$!%*#?&]{8,}$",
+              "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character "
+            )
+            .required("Required"),
+          repeatPassword: Yup.string().oneOf(
+            [Yup.ref("password")],
+            "Passwords must match"
+          ),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        <Form>
+          <label htmlFor="userName">Nazwa Użytkownika</label>
+          <Field
+            name="userName"
+            type="text"
+            placeholder="Wprowadź nazwę użytkownika"
+          />
+          <ErrorMessage name="userName" />
+          <label htmlFor="email">E-mail</label>
+          <Field name="email" type="email" placeholder="Wprowadź email" />
+          <ErrorMessage name="email" />
+          <label htmlFor="password">Hasło</label>
+          <Field name="password" type="password" placeholder="Wprowadź hasło" />
+          <ErrorMessage name="password" />
+          <label htmlFor="repeatPassword">Powtórz Hasło</label>
+          <Field
+            name="repeatPassword"
+            type="password"
+            placeholder="Powtórz hasło"
+          />
+          <ErrorMessage name="repeatPassword" />
+          <button type="submit">Wyślij</button>
+        </Form>
+      </Formik>
+    </>
   );
 };
 
