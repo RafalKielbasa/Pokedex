521ccac4dda9144b014c5b49e0ae2d6cabeb35c7
Mateusz Kowa <m.kowa2424@gmail.com>
2023-05-15 14:15:06 +0200

    rejestracja ver 1.2

--- pokedex/src/Pages/RegisterPage.js
+++ pokedex/src/Pages/RegisterPage.js
@@ -8,62 +8,74 @@ const RegisterPage = () => {
   const regPasword =
     /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
   return (
-    <>
-      <Formik
-        initialValues={{
-          userName: "",
-          email: "",
-          password: "",
-          repeatPassword: "",
-        }}
-        validationSchema={Yup.object({
-          userName: Yup.string()
-            .max(15, "Must be 15 characters or less")
-            .required("Required"),
-          email: Yup.string()
-            .email("Invalid email address")
-            .required("Required"),
-          password: Yup.string()
-            .matches(
-              regPasword,
-              "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character "
-            )
-            .required("Required"),
-          repeatPassword: Yup.string()
-            .oneOf([Yup.ref("password")], "Passwords must match")
-            .required("Required"),
-        })}
-        onSubmit={(values, { setSubmitting }) => {
-          postData("users", values);
-          setSubmitting(false);
-          enqueueSnackbar("Rejestracja udana", { variant: "success" });
-        }}
-      >
+    <Formik
+      initialValues={{
+        userName: "",
+        email: "",
+        password: "",
+        repeatPassword: "",
+      }}
+      validationSchema={Yup.object({
+        userName: Yup.string()
+          .max(15, "Must be 15 characters or less")
+          .required("Required"),
+        email: Yup.string().email("Invalid email address").required("Required"),
+        password: Yup.string()
+          .matches(
+            regPasword,
+            "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character "
+          )
+          .required("Required"),
+        repeatPassword: Yup.string()
+          .oneOf([Yup.ref("password")], "Passwords must match")
+          .required("Required"),
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        postData("users", values);
+        setSubmitting(false);
+        enqueueSnackbar("Rejestracja udana", { variant: "success" });
+      }}
+    >
+      {({ isSubmitting }) => (
         <Form>
-          <label htmlFor="userName">Nazwa Użytkownika</label>
-          <Field
-            name="userName"
-            type="text"
-            placeholder="Wprowadź nazwę użytkownika"
-          ></Field>
-          <ErrorMessage name="userName" />
-          <label htmlFor="email">E-mail</label>
-          <Field name="email" type="email" placeholder="Wprowadź email" />
-          <ErrorMessage name="email" />
-          <label htmlFor="password">Hasło</label>
-          <Field name="password" type="password" placeholder="Wprowadź hasło" />
-          <ErrorMessage name="password" />
-          <label htmlFor="repeatPassword">Powtórz Hasło</label>
-          <Field
-            name="repeatPassword"
-            type="password"
-            placeholder="Powtórz hasło"
-          />
-          <ErrorMessage name="repeatPassword" />
-          <button type="submit">Wyślij</button>
+          <div>
+            <label htmlFor="userName">Nazwa Użytkownika</label>
+            <Field
+              name="userName"
+              type="text"
+              placeholder="Wprowadź nazwę użytkownika"
+            ></Field>
+            <ErrorMessage name="userName" />
+          </div>
+          <div>
+            <label htmlFor="email">E-mail</label>
+            <Field name="email" type="email" placeholder="Wprowadź email" />
+            <ErrorMessage name="email" />
+          </div>
+          <div>
+            <label htmlFor="password">Hasło</label>
+            <Field
+              name="password"
+              type="password"
+              placeholder="Wprowadź hasło"
+            />
+            <ErrorMessage name="password" />
+          </div>
+          <div>
+            <label htmlFor="repeatPassword">Powtórz Hasło</label>
+            <Field
+              name="repeatPassword"
+              type="password"
+              placeholder="Powtórz hasło"
+            />
+            <ErrorMessage name="repeatPassword" />
+          </div>
+          <button type="submit" disabled={isSubmitting}>
+            Wyślij
+          </button>
         </Form>
-      </Formik>
-    </>
+      )}
+    </Formik>
   );
 };
 
