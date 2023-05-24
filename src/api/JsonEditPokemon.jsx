2fd3c2e961a6491c3cc0fed4b28c078809557f70
Wiktor Ostrach <ostrach109@gmail.com>
2023-05-24 22:23:50 +0200

    ...

--- src/api/JsonEditPokemon.jsx
+++ src/api/JsonEditPokemon.jsx
@@ -1,28 +1,28 @@
-import axios from "axios";
+import axios from 'axios'
 
 export const JsonEditPokemon = (pokemonData, newValues) => {
-  axios
-    .post(`http://localhost:3001/editedPokemon/`, {
-      id: pokemonData?.id,
-      sprite: pokemonData?.sprite,
-      name: newValues.pokemonName,
-      weight: newValues.weight,
-      ability: newValues.ability,
-      height: newValues.height,
-      base_experience: newValues.baseExperience,
-    })
-    .catch((error) => {
-      if (error.response.status === 500) {
-        axios.put(`http://localhost:3001/editedPokemon/${pokemonData?.id}`, {
-          id: pokemonData?.id,
-          sprite: pokemonData?.sprite,
-          name: newValues.pokemonName,
-          weight: newValues.weight,
-          ability: newValues.ability,
-          height: newValues.height,
-          base_experience: newValues.baseExperience,
-        });
-      }
-    });
-  return;
-};
+	axios
+		.post(`http://localhost:3001/editedPokemon/`, {
+			id: pokemonData?.id,
+			sprite: pokemonData?.sprite,
+			name: newValues.pokemonName,
+			weight: newValues.weight,
+			ability: newValues.ability,
+			height: newValues.height,
+			base_experience: newValues.baseExperience,
+		})
+		.catch(error => {
+			if (error.response.status === 500) {
+				axios.put(`http://localhost:3001/editedPokemon/${pokemonData?.id}`, {
+					id: pokemonData?.id,
+					sprite: pokemonData?.sprite,
+					name: newValues.pokemonName,
+					weight: newValues.weight,
+					ability: newValues.ability,
+					height: newValues.height,
+					base_experience: newValues.baseExperience,
+				})
+			}
+		})
+	return
+}
