736de7b29310d8507c397347d7241cf0a90d7e54
Poul <paweljarujaruszewski@gmail.com>
2023-05-31 22:35:41 +0200

    fix

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,26 +1,9 @@
 import { DefaultLayout } from "../components/layouts/Default/DefaultLayout";
 import { HomePageWrapper } from "../components/wrappers/HomePageWrapper/HomePageWrapper";
-import {
-  fetchAllPokemon,
-  fetchEachPokemon,
-  pokemonMapper,
-  saveToDb,
-} from "../services/api";
 import { useAllPokemonQuery } from "../hooks/useAllPokemon";
 
 export const HomePage = () => {
   const { data } = useAllPokemonQuery();
-
-  // useEffect(() => {
-  //   fetchAllPokemon().then((res) => {
-  //     res.data.results.map((pokemon) => {
-  //       return fetchEachPokemon(pokemon.url).then((res) => {
-  //         const pokemon = pokemonMapper(res);
-  //         saveToDb(pokemon);
-  //       });
-  //     });
-  //   });
-  // }, []);
 
   return (
     <DefaultLayout>
