d509a50ef72b4b660fbf0828147c579c5dd08a2f
mr-fox93 <lisieckikamil93@gmail.com>
2023-06-25 19:16:48 +0200

    EditPage now with styled-comp work 100%

--- src/EditPage.js
+++ src/EditPage.js
@@ -4,6 +4,66 @@ import { useForm } from "react-hook-form";
 import { GlobalContext } from "./context/global";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const StyledLink = styled(Link)`
+  margin-right: 5px;
+  font-weight: bold;
+  color: black;
+`;
+
+const Body = styled.body`
+  min-height: 100vh;
+  background-color: ${({ swich }) => (swich ? "#394867" : "#d4f1f4")};
+`;
+
+const Wrapper = styled.div`
+  padding: 2rem;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+`;
+
+const Select = styled.select`
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Button = styled.button`
+  width: 48%;
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  background-color: #007bff;
+  color: white;
+`;
+
+const Info = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-weight: bold;
+`;
 
 const schema = yup.object().shape({
   name: yup.string().required(),
@@ -14,7 +74,7 @@ const schema = yup.object().shape({
 });
 
 const EditPage = () => {
-  const { pokemons, setPokemons } = useContext(GlobalContext);
+  const { pokemons, setPokemons, userInfo, swich } = useContext(GlobalContext);
   const [selectedPokemon, setSelectedPokemon] = useState(null);
   const {
     register,
@@ -101,58 +161,68 @@ const EditPage = () => {
   };
 
   return (
-    <div>
-      <h1>Edit/Create Pokemon</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <select name="pokemon" onChange={selectPokemon}>
-          {pokemons.map((pokemon) => (
-            <option key={pokemon.id} value={pokemon.name}>
-              {pokemon.name}
-            </option>
-          ))}
-        </select>
+    <Body swich={swich}>
+      {userInfo ? (
+        <Wrapper>
+          <h1>Edit/Create Pokemon</h1>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Select name="pokemon" onChange={selectPokemon}>
+              <option value="">Choose a Pokemon</option>
+              {pokemons.map((pokemon) => (
+                <option key={pokemon.id} value={pokemon.name}>
+                  {pokemon.name}
+                </option>
+              ))}
+            </Select>
 
-        <input
-          defaultValue={selectedPokemon?.name}
-          {...register("name")}
-          placeholder="Name"
-        />
-        {errors.name && <p>{errors.name.message}</p>}
+            <Input
+              defaultValue={selectedPokemon?.name}
+              {...register("name")}
+              placeholder="Name"
+            />
+            {errors.name && <p>{errors.name.message}</p>}
 
-        <input
-          defaultValue={selectedPokemon?.weight}
-          {...register("weight")}
-          placeholder="Weight"
-        />
-        {errors.weight && <p>{errors.weight.message}</p>}
+            <Input
+              defaultValue={selectedPokemon?.weight}
+              {...register("weight")}
+              placeholder="Weight"
+            />
+            {errors.weight && <p>{errors.weight.message}</p>}
 
-        <input
-          defaultValue={selectedPokemon?.height}
-          {...register("height")}
-          placeholder="Height"
-        />
-        {errors.height && <p>{errors.height.message}</p>}
+            <Input
+              defaultValue={selectedPokemon?.height}
+              {...register("height")}
+              placeholder="Height"
+            />
+            {errors.height && <p>{errors.height.message}</p>}
 
-        <input
-          defaultValue={selectedPokemon?.ability}
-          {...register("ability")}
-          placeholder="Ability"
-        />
-        {errors.ability && <p>{errors.ability.message}</p>}
+            <Input
+              defaultValue={selectedPokemon?.ability}
+              {...register("ability")}
+              placeholder="Ability"
+            />
+            {errors.ability && <p>{errors.ability.message}</p>}
 
-        <input
-          defaultValue={selectedPokemon?.base_exp}
-          {...register("base_exp")}
-          placeholder="Base Experience"
-        />
-        {errors.base_exp && <p>{errors.base_exp.message}</p>}
-
-        <button type="submit">SAVE</button>
-      </form>
-      <button type="button" onClick={saveAsNew}>
-        SAVE AS NEW
-      </button>
-    </div>
+            <Input
+              defaultValue={selectedPokemon?.base_exp}
+              {...register("base_exp")}
+              placeholder="Base Experience"
+            />
+            {errors.base_exp && <p>{errors.base_exp.message}</p>}
+            <ButtonContainer>
+              <Button type="submit">SAVE</Button>
+              <Button type="button" onClick={saveAsNew}>
+                SAVE AS NEW
+              </Button>
+            </ButtonContainer>
+          </Form>
+        </Wrapper>
+      ) : (
+        <Info>
+          <StyledLink to="/login">LOG IN</StyledLink>to see this page.
+        </Info>
+      )}
+    </Body>
   );
 };
 
